1f0fccc4c8418054bc84860f458f2981f9fd8b1b
SickAssApp <lilithsquall2001@gmail.com>
2017-11-26 00:06:08 +0800

    rename the url of Heroku
    commit this to check if the repo connection is losted or not.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'Meh'});
+    res.send({hi: 'Rename the app'});
 });
 
 const PORT = process.env.PORT || 5000;
