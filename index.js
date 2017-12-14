12c0569f3b3a5e5834f0a5aa7d73439c58fb66ff
SickAssApp <lilithsquall2001@gmail.com>
2017-12-14 18:56:23 +0800

    Add online payment and client build

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 
-require('./models/user');
-require('./services/passport');
+require("./models/user");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,8 +23,20 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === 'production'){
+  // Serve up production assets
+  app.use(express.static('client/build'));
+
+  // Serv up index.html if doesn't recognize the rout
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
-console.log('Running on:' + PORT);
+console.log("Running on:" + PORT);
 app.listen(PORT);
