19e6c9a76eabdd231cab1d8cbeade81cacca961b
SickAssApp <lilithsquall2001@gmail.com>
2018-01-10 01:30:23 +0800

    Add survey form

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const bodyParser = require("body-parser");
-const keys = require("./config/keys");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 
-require("./models/user");
-require("./services/passport");
+require('./models/user');
+require('./models/Survey');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
@@ -23,10 +24,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./routes/authRoutes")(app);
-require("./routes/billingRoutes")(app);
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   // Serve up production assets
   app.use(express.static('client/build'));
 
@@ -34,9 +36,9 @@ if (process.env.NODE_ENV === 'production'){
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  });
 }
 
 const PORT = process.env.PORT || 5000;
-console.log("Running on:" + PORT);
+console.log('Running on:' + PORT);
 app.listen(PORT);
