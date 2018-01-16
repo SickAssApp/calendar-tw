a878989f0449a9acd899a65128ee5994554e50ab
SickAssApp <lilithsquall2001@gmail.com>
2018-01-16 15:59:20 +0800

    Edit form field

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,33 @@
-// SurevyNew shows SurveyForm and SurveyFormReview 
-import React,  { Component } from 'react';
+// SurevyNew shows SurveyForm and SurveyFormReview
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-    render(){
-        return(
-            <div>
-                <SurveyForm />
-            </div>
-        )
+  state = { showFormReview: false };
+
+  rederContent() {
+    if (this.state.showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
     }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
+  render() {
+    return <div>{this.rederContent()}</div>;
+  }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
